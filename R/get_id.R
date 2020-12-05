8470b0afbe75258249db78585dcc819986ff5d32
Jim Albert <albertcb1@gmail.com>
2020-12-05 18:58:08 -0500

    Minor changes

--- R/get_id.R
+++
@@ -1,6 +0,0 @@
-get_id <- function(name){
-  names <- unlist(strsplit(name, " "))
-  playerid_lookup(last_name = names[2],
-                  first_name = names[1]) %>%
-    dplyr::select(mlbam_id) %>% top_n(-1) %>% pull()
-}
